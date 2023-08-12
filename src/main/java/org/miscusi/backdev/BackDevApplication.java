f909673e0efd7e7eef77cd1733d4de14e1f7421b
DarioGiusino <dario.giusino@gmail.com>
2023-08-12 12:07:42 +0200

    first commit

---
+++ src/main/java/org/miscusi/backdev/BackDevApplication.java
@@ -0,0 +1,13 @@
+package org.miscusi.backdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackDevApplication.class, args);
+	}
+
+}
