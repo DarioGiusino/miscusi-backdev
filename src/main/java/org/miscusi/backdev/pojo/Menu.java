ec076e0c413d941f3c556974f79f23e629f25b43
DarioGiusino <dario.giusino@gmail.com>
2023-08-12 18:41:40 +0200

    type in desc menu

--- src/main/java/org/miscusi/backdev/pojo/Menu.java
+++ src/main/java/org/miscusi/backdev/pojo/Menu.java
@@ -1,5 +1,6 @@
 package org.miscusi.backdev.pojo;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -14,6 +15,7 @@ public class Menu {
 
     private String name;
 
+    @Column(columnDefinition = "TEXT")
     private String description;
 
     public Menu() {
